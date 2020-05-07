abd863d030a1736e0857b867c1a2aac8263cc9ce
olwikia <63187508+olwikia@users.noreply.github.com>
2020-05-07 18:15:16 +0200

    Tworzenie zasobów

---
+++ app/src/main/java/com/example/gg39998_lab6_http/Config.java
@@ -0,0 +1,4 @@
+package com.example.gg39998_lab6_http;
+
+public class Config {
+}
---
+++ app/src/main/java/com/example/gg39998_lab6_http/inRow.java
@@ -0,0 +1,14 @@
+package com.example.gg39998_lab6_http;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inRow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_row);
+    }
+}
